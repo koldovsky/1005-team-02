0752419a4d1be4d30aa8502607da83c05354f512
Yuriy <yuriy.dmyterko@gmail.com>
2023-04-28 13:46:23 +0000

    added carousel to partials/new-arrivals.partial.html

--- js/html-data-include.js
+++ js/html-data-include.js
@@ -1,6 +1,7 @@
 document.addEventListener("partialsLoaded", () => {
     // тут треба поміняти на ваші JS файли
     import('./header.js');
+    import('./new-arrivals.js');
     import('./header-clock.js');
     import('./product-list.js');
     import('./products-carousel.js');
---
+++ js/new-arrivals.js
@@ -0,0 +1,43 @@
+const slides = [
+    '<div><img src="img/new_arrival_sticks.jpg" alt="Sticks"></div>',
+    '<div><img src="img/new_arrival_toy.jpg" alt="Toy"></div>',
+    '<div><img src="img/new_arrival_leash.jpg" alt="Leash"></div>',
+    '<div><img src="img/new_arrival_bowl.jpg" alt="Bowl"></div>',
+    '<div><img src="img/new_arrival_butterfly.jpg" alt="Butterfly"></div>'
+];
+
+let currentSlide = 0;
+
+function showCurrentSlide() {
+    const slideContainer = document.querySelector('.products__carousel-slide');
+    slideContainer.innerHTML = slides[currentSlide];
+    if (window.innerWidth > 768) {
+        const secondSlideIdx = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+        slideContainer.innerHTML += slides[secondSlideIdx];
+        if (window.innerWidth > 1024) {
+            const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+            slideContainer.innerHTML += slides[thirdSlideIdx];
+        }
+    }
+}
+
+function nextSlide() {
+    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+    showCurrentSlide();
+}
+
+function prevSlide() {
+    currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1;
+    showCurrentSlide();
+}
+
+nextSlide();
+setInterval(nextSlide, 3000);
+
+const btnNext = document.querySelector('.products__carousel-btn-next');
+btnNext.addEventListener('click', nextSlide);
+
+const btnPrev = document.querySelector('.products__carousel-btn-prev');
+btnPrev.addEventListener('click', prevSlide);
+
+window.addEventListener('resize', showCurrentSlide);
