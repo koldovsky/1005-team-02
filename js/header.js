b6f7765ecbeb75a8ff50e871f37081eba1379cff
Wiret02 <chuwachok6@gmail.com>
2023-05-01 09:43:27 +0300

    rename main js file to index.js, add id to sections, click on menuItem close menu and move to that section

--- js/header.js
+++ js/header.js
@@ -1,7 +1,17 @@
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.burger__menu');
+const menuItem = document.querySelectorAll('.burger__item');
+
+console.log(menuItem);
 
 burger.addEventListener('click', function() {
-    this.classList.toggle('active');
+    burger.classList.toggle('active');
     menu.classList.toggle('active');
 });
+
+for (let i = 0; i < menuItem.length; i++) {
+    menuItem[i].addEventListener('click', function() {
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+}
---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("partialsLoaded", () => {
+    import('./header.js');
+    import('./new-arrivals.js');
+});
