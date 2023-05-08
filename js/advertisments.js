e976ff0c0898421303097ef09a26bd294782bd80
Zubyk <129189011+Hlib4yk@users.noreply.github.com>
2023-05-08 22:17:46 +0000

    carousel

--- js/advertisments.js
+++ js/advertisments.js
@@ -1,55 +1,37 @@
-(function () {
-    const slides = [
-      `
-      <div class="pet-food_carousel_slide">
-      <img class="pet-food_carousel_slide" src="img/applaws.png" alt="AllPaws" />
-      <img class="pet-food_carousel_slide" src="img/tigerino.png" alt="Tigerino" />
-      <img class="pet-food_carousel_slide" src="img/gourmet.png" alt="Gourmet" />
-      <img class="pet-food_carousel_slide" src="img/felix.png" alt="Felix" />
-      <img class="pet-food_carousel_slide" src="img/purizon.png" alt="Purizon" />
-      <img class="pet-food_carousel_slide" src="img/rocco.png" alt="Rocco" />
-      <img class="pet-food_carousel_slide" src="img/dokas.png" alt="Dokas" />
-      </div>
-      `,
-    ];
-  
-    let currentSlideIdx = 0;
-  
-    function renderSlide() {
-      const slideContainer = document.querySelector(".carousel_goods_slides");
-      slideContainer.innerHTML = slides[currentSlideIdx];
-      if (window.innerWidth > 768) {
-        const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-        slideContainer.innerHTML += slides[secondSlideIdx];
-        if (window.innerWidth > 991) {
-          const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
-          slideContainer.innerHTML += slides[thirdSlideIdx];
-          if (window.innerWidth > 992) {
-            const fourthSlideIdx = thirdSlideIdx + 1 >= slides.length ? 0 : thirdSlideIdx + 1;
-            slideContainer.innerHTML += slides[fourthSlideIdx];
-          }
-        }
-      }
+let slideIndex = 0;
+
+function showSlide() {
+  const slides = document.querySelectorAll(".pet-food_carousel_slide");
+  for (let i = 0; i < slides.length; i++) {
+    if (i === slideIndex) {
+      slides[i].style.display = "block";
+    } else {
+      slides[i].style.display = "none";
     }
- 
-    function nextSlide() {
-      currentSlideIdx =
-          currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-      renderSlide()
-    }
-  
-    function prevSlide() {
-      currentSlideIdx =
-          currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
-      renderSlide()
-    }
-        
-    renderSlide();
-  
-    const nextBtn = document.querySelector('.pet-food__arrow-right')
-    nextBtn.addEventListener('click', nextSlide)
-    const prevtBtn = document.querySelector('.pet-food__arrow-left')
-    prevtBtn.addEventListener('click', prevSlide)
-  
-    window.addEventListener('resize', renderSlide)
-  })();
+  }
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  const slides = document.querySelectorAll(".pet-food_carousel_slide");
+  if (slideIndex > slides.length - 1) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  showSlide();
+}
+
+const prevBtn = document.querySelector(".pet-food__arrow-left");
+const nextBtn = document.querySelector(".pet_food__arrow-right");
+
+prevBtn.addEventListener("click", () => {
+  changeSlide(-1);
+});
+
+nextBtn.addEventListener("click", () => {
+  changeSlide(1);
+});
+
+showSlide();
