33840881a37324d1b5f670ebd24ba50896250699
Zubyk <129189011+Hlib4yk@users.noreply.github.com>
2023-05-09 09:29:55 +0000

    carousel upd

--- js/advertisments.js
+++ js/advertisments.js
@@ -1,9 +1,9 @@
 let slideIndex = 0;
 
-function showSlide() {
+function showSlide(startIndex) {
   const slides = document.querySelectorAll(".pet-food_carousel_slide");
   for (let i = 0; i < slides.length; i++) {
-    if (i === slideIndex) {
+    if (i >= startIndex && i < startIndex + 6) {
       slides[i].style.display = "block";
     } else {
       slides[i].style.display = "none";
@@ -11,17 +11,18 @@ function showSlide() {
   }
 }
 
+
 function changeSlide(n) {
   slideIndex += n;
   const slides = document.querySelectorAll(".pet-food_carousel_slide");
-  if (slideIndex > slides.length - 1) {
-    slideIndex = 0;
-  }
   if (slideIndex < 0) {
     slideIndex = slides.length - 1;
+  } else if (slideIndex + 6 > slides.length) {
+    slideIndex = 0;
   }
-  showSlide();
+  showSlide(slideIndex);
 }
+
 
 const prevBtn = document.querySelector(".pet-food__arrow-left");
 const nextBtn = document.querySelector(".pet_food__arrow-right");
